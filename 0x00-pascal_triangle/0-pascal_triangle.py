0c1a7d17096e39880ff3aeed93ba9f0af7e0316e
Eric <eroija@gmail.com>
2024-07-25 15:06:37 +0300

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """A module for working with Pascal's triangle."""
 
-#!/usr/bin/python3
-"""
-function def pascal_triangle(n): that returns a list of lists of integers
-representing the Pascal's triangle of n
-"""
-
 
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal's
